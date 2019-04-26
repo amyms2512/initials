27083ea5da2662f5b6b2e1780601b9144c142625
Amy Starks <amystarks@Amy-Starkss-MacBook-2.local>
2019-04-25 20:09:20 -0600

    initial commit

---
+++ initials.py
@@ -0,0 +1,13 @@
+def get_initials(fullname):
+    """ Given a person's name, returns the person's initials (uppercase) """
+    """Use slice to grab the first letter of each part of the name and analyze it."""
+    """THe first part of the name will start with the first letter. Other parts of the name will start after a blank space."""
+    """Use the method that changes letters to uppercase"""
+    """Iterate through all parts of the name to capture all initials"""
+    """Return all initials"""
+    """Print them"""
+    
+    # TODO your code here
+    print("Hello World!")
+    #for initials in (name):
+get_initials("fullname")
